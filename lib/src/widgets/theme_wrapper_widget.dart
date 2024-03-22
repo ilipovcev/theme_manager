28c4c738ffb14207758884dd387d45ef13a460cc
ilipa <ilya.lipovcev@bk.ru>
2024-03-22 13:15:05 +0300

    listen device brightness changes

--- lib/src/widgets/theme_wrapper_widget.dart
+++ lib/src/widgets/theme_wrapper_widget.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:theme_manager/src/controller/theme_bloc.dart';
+import 'package:theme_manager/src/src.dart';
 
 /// Wrap your app with this widget to use the theme bloc.
-class ThemeWrapperWidget extends StatelessWidget {
+class ThemeWrapperWidget extends StatefulWidget {
   const ThemeWrapperWidget({
     super.key,
     required this.builder,
@@ -15,10 +17,31 @@ class ThemeWrapperWidget extends StatelessWidget {
   ) builder;
 
   @override
+  State<ThemeWrapperWidget> createState() => _ThemeWrapperWidgetState();
+}
+
+class _ThemeWrapperWidgetState extends State<ThemeWrapperWidget> {
+  @override
+  void initState() {
+    super.initState();
+
+    final dispatcher = SchedulerBinding.instance.platformDispatcher;
+
+    // This callback is called every time the brightness changes.
+    dispatcher.onPlatformBrightnessChanged = () {
+      final brightness = dispatcher.platformBrightness;
+
+      context.switchThemeMode(
+        brightness == Brightness.dark ? ThemeMode.dark : ThemeMode.light,
+      );
+    };
+  }
+
+  @override
   Widget build(BuildContext context) => BlocProvider(
         create: (context) => ThemeBloc(),
         child: BlocBuilder<ThemeBloc, ThemeState>(
-          builder: builder,
+          builder: widget.builder,
         ),
       );
 }
